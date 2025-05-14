48e3c931334c3454c809ca3b849e682ef19ca560
anonymous-cloud <57187026+anonymous-cloud@users.noreply.github.com>
2025-05-14 17:51:30 +0530

    server created

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
-console.log("hello");
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Express!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
